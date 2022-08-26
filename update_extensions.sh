617e05f70ec402f9294a42cdd9a782c80f9e0694
Benjamin Cortés <48661799+cortesjpb@users.noreply.github.com>
2022-08-26 09:43:56 -0300

    update extensions files added

---
+++ update_extensions.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+code --user-data-dir=/home/a310726/.vscode-profiles/base/data/ --extensions-dir=/home/a310726/.vscode-profiles/base/extensions/ --list-extensions > base/extensions/extensions.txt
+code --user-data-dir=/home/a310726/.vscode-profiles/java/data/ --extensions-dir=/home/a310726/.vscode-profiles/java/extensions/ --list-extensions > java/extensions/extensions.txt
+code --user-data-dir=/home/a310726/.vscode-profiles/python/data/ --extensions-dir=/home/a310726/.vscode-profiles/python/extensions/ --list-extensions > python/extensions/extensions.txt
+code --user-data-dir=/home/a310726/.vscode-profiles/react/data/ --extensions-dir=/home/a310726/.vscode-profiles/react/extensions/ --list-extensions > react/extensions/extensions.txt
