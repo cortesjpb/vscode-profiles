78c4a48e1606a409d240b3d41c657eaea80f0a7e
Benjamin Cortés <48661799+cortesjpb@users.noreply.github.com>
2022-08-26 09:47:21 -0300

    update extensions updated

--- update_extensions.sh
+++ update_extensions.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-code --user-data-dir=/home/a310726/.vscode-profiles/base/data/ --extensions-dir=/home/a310726/.vscode-profiles/base/extensions/ --list-extensions > base/extensions/extensions.txt
-code --user-data-dir=/home/a310726/.vscode-profiles/java/data/ --extensions-dir=/home/a310726/.vscode-profiles/java/extensions/ --list-extensions > java/extensions/extensions.txt
-code --user-data-dir=/home/a310726/.vscode-profiles/python/data/ --extensions-dir=/home/a310726/.vscode-profiles/python/extensions/ --list-extensions > python/extensions/extensions.txt
-code --user-data-dir=/home/a310726/.vscode-profiles/react/data/ --extensions-dir=/home/a310726/.vscode-profiles/react/extensions/ --list-extensions > react/extensions/extensions.txt
+code --user-data-dir=/{path/to/folder}/.vscode-profiles/base/data/ --extensions-dir=/{path/to/folder}/.vscode-profiles/base/extensions/ --list-extensions > base/extensions/extensions.txt
+code --user-data-dir=/{path/to/folder}/.vscode-profiles/java/data/ --extensions-dir=/{path/to/folder}/.vscode-profiles/java/extensions/ --list-extensions > java/extensions/extensions.txt
+code --user-data-dir=/{path/to/folder}/.vscode-profiles/python/data/ --extensions-dir=/{path/to/folder}/.vscode-profiles/python/extensions/ --list-extensions > python/extensions/extensions.txt
+code --user-data-dir=/{path/to/folder}/.vscode-profiles/react/data/ --extensions-dir=/{path/to/folder}/.vscode-profiles/react/extensions/ --list-extensions > react/extensions/extensions.txt
